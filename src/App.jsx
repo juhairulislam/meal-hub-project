2de4d502e016db2cff6e6aef500ac1b60675737d
juhairulislam <juhairulislam2018@gmail.com>
2026-03-16 07:56:42 +0600

    understand react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [num, setNum] = useState(0);
+
+  const changeNumber = () =>{
+
+    setNum(num+1) ;
+  }
+
   return (
     <>
+    <h1>{num}</h1>
+    <br />
+    <button onClick={changeNumber}>Click for change num</button>
      
     </>
   )
